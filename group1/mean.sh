a6017098f3a37d2cd60b47cec5163b6a31520ff2
OliverXUZY <zyxu@DESKTOP-3KSQN15.localdomain>
2019-10-17 15:14:43 -0500

    update

--- group1/mean.sh
+++ group1/mean.sh
@@ -17,6 +17,31 @@ col=$1; # file=$2
 
 ( cut -d"," -f$col < $file | tail -n+2 ) | awk '{sum+=$1}END{print sum/NR}'
 
+
+# alternative method  appendix:
+cat num |
+    {
+	sum=0
+	while read n ; do
+	    sum=$(($sum+$n))
+	done
+
+	echo $sum
+    }
+
+## for loop
+count=0
+sum=0
+
+# loop
+for i in $data; do
+    sum=$(echo "scale=1; $sum + $i" | bc)
+    count=$(($count+1))
+done
+
+# calculate mean
+mean=$(echo "scale=1; $sum/$count" | bc)
+
 #while read line; do
 #    {sum += line; n++}
 #   # echo $line
